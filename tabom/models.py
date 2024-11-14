d158994224dbb1a7e3966d3033358eed0be95d5a
SW <aliwo@naver.com>
2024-11-14 14:49:30 +0900

    implement do_like

--- tabom/models.py
+++ tabom/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
 
 
-class User(models.Model):
+class User(BaseModel):
     # django 기본 모델이 auth_user 가 있습니다. 원래는 그걸 사용하는게 맞습니다만
     # 단순화를 위해 user model 도 그냥 생성하겠습니다.
     name = models.CharField(max_length=50)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Article(models.Model):
+class Article(BaseModel):
     title = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Like(models.Model):
+class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
---
+++ tabom/services/like_service.py
@@ -0,0 +1,22 @@
+from tabom.models import Like
+
+
+def do_like(user_id: int, article_id: int) -> Like:
+    """
+    get 을 할 필요가 없는 이유
+        - Foreign key contraint 가 있기 때문에
+
+    user_id:
+        - 이 id 의 user 가 실제로 있는 경우
+        - 실제로 없는 경우
+
+    article_id:
+        - 이 id 의 article 이 실제로 있는 경우
+        - 실제로 없는 경우
+
+    - Foreign key contraint 에 의해서 "실제로 없는 경우는" insert 되지 않는다.
+        - db 가 지켜줍니다. (정합성을)
+        - 따라서 지금은 크게 신경 안써도 됩니다.
+
+    """
+    return Like.objects.create(user_id=user_id, article_id=article_id)
