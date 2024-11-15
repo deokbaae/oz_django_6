1fb46b3e846caa464f9ac5f48001fe4c020cd995
SW <aliwo@naver.com>
2024-11-15 15:12:42 +0900

    undo_like

--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -2,7 +2,7 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from tabom.models import Article, Like, User
-from tabom.services.like_service import do_like
+from tabom.services.like_service import do_like, undo_like
 
 
 class TestLikeService(TestCase):
@@ -31,3 +31,48 @@ class TestLikeService(TestCase):
         # assertRaises 의 동작: Exception 이 발생하면 통과, 아무일도 안 일어나면 AssertionError 를 일으킨다.
         with self.assertRaises(IntegrityError):
             do_like(user.id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_user_does_not_exist(self) -> None:
+        # Given
+        invalid_user_id = 9988
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(invalid_user_id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_article_does_not_exist(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        invalid_article_id = 9988
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(user.id, invalid_article_id)
+
+    def test_like_count_should_increase(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        # When
+        do_like(user.id, article.id)
+
+        # Then
+        result_article = Article.objects.get(id=article.id)
+        self.assertEqual(1, article.like_set.count())
+
+    def test_a_user_can_undo_like(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        like = Like.objects.create(user_id=user.id, article_id=article.id)
+
+        # When
+        undo_like(user.id, article.id)
+
+        # Then
+        with self.assertRaises(Like.DoesNotExist):
+            Like.objects.get(id=like.id)
+
+        # then 절에서 DoesNotExist 로 검사하는거 말고 다른 방법이 있지 않을까?
