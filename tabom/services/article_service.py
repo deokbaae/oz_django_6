ec7160fafe4844e240ac17258c1fa49f343c0f01
SW <aliwo@naver.com>
2024-11-18 14:12:54 +0900

    리스트 조회 기능 구현

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,5 +1,12 @@
+from django.db.models import QuerySet
+
 from tabom.models import Article
 
 
 def get_an_article(article_id: int) -> Article:
     return Article.objects.get(id=article_id)
+
+
+# Article 이 들어있는 쿼리셋을 반환할 것이다.
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id")[offset : offset + limit]
--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
-from tabom.models import Article
-from tabom.services.article_service import get_an_article
+from tabom.models import Article, Like, User
+from tabom.services.article_service import get_an_article, get_article_list
 
 
 class TestArticleService(TestCase):
@@ -24,3 +24,24 @@ class TestArticleService(TestCase):
         # Expect
         with self.assertRaises(Article.DoesNotExist):
             get_an_article(invalid_article_id)
+
+    def test_get_article_list_should_prefetch_likes(self) -> None:
+        # Given
+        user = User.objects.create(name="user1")
+        articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]  # 20 개의 article 생성
+        Like.objects.create(user_id=user.id, article_id=articles[-1].id)
+
+        # When
+        result_articles = get_article_list(0, 10)
+
+        # Then
+        with self.assertNumQueries(2):
+
+            # 실제로 쿼리가 나가는 지점. (왜? len() 으로 감쌋기 때문에,
+            # queryset 은 evaluate 될 때 sql 이 실행되니까)
+            self.assertEqual(len(result_articles), 10)  # 길이가 10개가 맞는지
+
+            self.assertEqual(1, result_articles[0].like_set.count())
+
+            # 내림차순대로 10개가 가져와진 것이 맞는지
+            self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
